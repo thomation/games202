c6931d825db7008cdaed1f28fa11e04cd1fda552
thomation <thomation@126.com>
2021-05-01 17:24:05 +0800

    Pass precompute value to shader

--- Assignment2/homework2/src/materials/PRTMaterial.js
+++ Assignment2/homework2/src/materials/PRTMaterial.js
@@ -2,9 +2,22 @@ class PRTMaterial extends Material {
 
     constructor(color, translate, scale, vertexShader, fragmentShader) {
         console.log('PRTMaterial');
-
+        let envlight = precomputeL[guiParams.envmapId];
+        console.log(envlight);
+        var mr = new Float32Array(envlight.length);
+        var mg = new Float32Array(envlight.length);
+        var mb = new Float32Array(envlight.length);
+        for(let i = 0; i < envlight.length; i ++) {
+            mr[i] = envlight[i][0];
+            mg[i] = envlight[i][1];
+            mb[i] = envlight[i][2];
+        }
+        console.log(mr);
         super({
             'uSampler': { type: 'texture', value: color },
+            'uPrecomputeLR':{type: 'matrix3fv', value: mr}, 
+            'uPrecomputeLG':{type:'matrix3fv', value: mg},
+            'uPrecomputeLB':{type:'matrix3fv', value: mb},
         }, [
             'aPrecomputeLT'
         ], vertexShader, fragmentShader, null);
@@ -12,14 +25,8 @@ class PRTMaterial extends Material {
 }
 
 async function buildPRTMaterial(color, translate, scale, vertexPath, fragmentPath) {
-
-    console.log('build prt material');
-    console.log(precomputeL[guiParams.envmapId]);
-    console.log(precomputeLT[guiParams.envmapId]);
     let vertexShader = await getShaderString(vertexPath);
     let fragmentShader = await getShaderString(fragmentPath);
-
-    console.log('build prt material');
     return new PRTMaterial(color, translate, scale, vertexShader, fragmentShader);
 
 }
--- Assignment2/homework2/src/shaders/PRTShader/PRTVertex.glsl
+++ Assignment2/homework2/src/shaders/PRTShader/PRTVertex.glsl
@@ -4,15 +4,25 @@ attribute mat3 aPrecomputeLT;
 uniform mat4 uModelMatrix;
 uniform mat4 uViewMatrix;
 uniform mat4 uProjectionMatrix;
+uniform mat3 uPrecomputeLR;
+uniform mat3 uPrecomputeLG;
+uniform mat3 uPrecomputeLB;
 
 varying vec3 vColor;
 
+float compute_color(mat3 precomputeL)
+{
+  float color = 0.0;
+  color += dot(aPrecomputeLT[0], precomputeL[0]);
+  color += dot(aPrecomputeLT[1], precomputeL[1]);
+  color += dot(aPrecomputeLT[2], precomputeL[2]);
+  return color;
+}
 void main(void) {
 
   gl_Position = uProjectionMatrix * uViewMatrix * uModelMatrix *
                 vec4(aVertexPosition, 1.0);
-  vColor.x = dot(aPrecomputeLT[0] , vec3(1.0, 1.0, 1.0));
-  vColor.y = dot(aPrecomputeLT[1] , vec3(1.0, 1.0, 1.0));
-  vColor.z = dot(aPrecomputeLT[2] , vec3(1.0, 1.0, 1.0));
-
+  vColor.x = compute_color(uPrecomputeLR);
+  vColor.y = compute_color(uPrecomputeLG);
+  vColor.z = compute_color(uPrecomputeLB);
 }
